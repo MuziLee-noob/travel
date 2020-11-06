a31f6bf1c6eb23d505c6adf0e6db467d6945e508
lijinhe <lijinhe@aisino.com>
2020-11-06 17:44:51 +0800

    列表性能优化

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,19 +17,24 @@
             v-for="item of hot"
             :key="item.id"
           >
-            <div class="button">{{item.name}}</div>
+            <div class="button">{{ item.name }}</div>
           </div>
         </div>
       </div>
-      <div class="area" v-for="(item, key) of cities" :key="key">
-        <div class="title border-topbottom">{{key}}</div>
+      <div
+        class="area"
+        v-for="(item, key) of cities"
+        :key="key"
+        :ref="key"
+      >
+        <div class="title border-topbottom">{{ key }}</div>
         <div class="item-list">
           <div
             class="item border-bottom"
             v-for="innerItem of item"
             :key="innerItem.id"
           >
-            {{innerItem.name}}
+            {{ innerItem.name }}
           </div>
         </div>
       </div>
@@ -39,14 +44,24 @@
 
 <script>
 import Bscroll from 'better-scroll'
+
 export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
@@ -56,11 +71,14 @@ export default {
 .border-topbottom
   &:before
     border-color: #ccc
+
   &:after
     border-color: #ccc
+
 .border-bottom
   &:before
     border-color: #ccc
+
 .list
   overflow: hidden
   position: absolute
@@ -68,24 +86,29 @@ export default {
   left: 0
   right: 0
   bottom: 0
+
   .title
     line-height: .54rem
     background: #eee
     padding-left: .2rem
     color: #666
     font-size: .26rem
+
   .button-list
     overflow: hidden
     padding: .1rem .6rem .1rem .1rem
+
     .button-wrapper
       float: left
       width: 33.33%
+
       .button
         margin: .1rem
         padding: .1rem 0
         text-align: center
         border: .02rem solid #ccc
         border-radius: .06rem
+
   .item-list
     .item
       line-height: .76rem
